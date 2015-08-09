5306eb342aa2cbc82fe41918d4a3a4ec0dd8f3d8
Yanbin Zhang <yanbinzhang.pe@gmail.com>
2015-08-08 22:23:50 -0500

    Add elpa archieves.

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;;;; Package archives
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+
 ;;;; House Keeping
 ;;; Start server
 (server-start)
