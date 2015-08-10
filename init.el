ebb4106da3719ced299fa3a21aa9e4d283a8818f
Yanbin Zhang <yanbinzhang.pe@gmail.com>
2015-08-09 22:05:24 -0500

    Setup dired.

--- init.el
+++ init.el
@@ -4,24 +4,43 @@
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
-;; Icicles
+;;;; Load essential packages
+;;; Icicles
 (add-to-list 'load-path "~/.emacs.d/elpa/icicles-20150804.1406")
 (require 'icicles)
 (icy-mode 1)
 
-;; LaCarte
+;;; LaCarte
 (add-to-list 'load-path "~/.emacs.d/elpa/lacarte-20150104.2350")
 (require 'lacarte)
 (global-set-key [?\e ?\M-x] 'lacarte-execute-command)
 (global-set-key [?\M-`]     'lacarte-execute-menu-command)
 (global-set-key [f10]       'lacarte-execute-menu-command)
 
-;; Dired+
+;;; Dired+
+(add-to-list 'load-path "/home/yanbin/emacsd/elpa/dired+-20150730.1406")
+;; Reuse dired buffer
+(diredp-toggle-find-file-reuse-dir 1)
 ;; This is not needed for emacs24.4
 (add-to-list 'load-path "/home/yanbin/.emacs.d/extra")
 (require 'dired-details)
 (dired-details-install)
+;; Change some dired keybindings
+(add-hook 'dired-mode-hook
+          (lambda ()
+            ;; use "u" to move to parent directory, use "r" to remove mark
+            (local-set-key (kbd "u") 'dired-up-directory)
+            (local-set-key (kbd "r") 'dired-unmark)
+            ))
 
+
+;;;; Set Appearance
+;;; Font
+;; The following font is in file ~/elisp/courR18.pcf.gz
+;; (set-default-font "-adobe-courier-medium-r-normal--18-180-75-75-m-110-iso8859-1")
+;;; Color
+(load-theme 'calm-forest t)
+(setq color-theme-is-global t)
 
 ;;;; House Keeping
 ;;; Start server
@@ -30,17 +49,6 @@
 (desktop-save-mode 1)
 ;;; Use y and n instead of yes and no
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-;;;; Set Appearance
-;;; Font
-;; The following font is in file ~/elisp/courR18.pcf.gz
-;; (set-default-font "-adobe-courier-medium-r-normal--18-180-75-75-m-110-iso8859-1")
-;;; Color
-;; (require 'color-theme)
-;; (setq color-theme-is-global t)
-;; (color-theme-calm-forest)
-(load-theme 'calm-forest t)
-(setq color-theme-is-global t)
 
 ;; Show colume number in the bar
 (setq column-number-mode t)
